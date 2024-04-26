184ff7bcfdfd429eda46818d649fcef09377eefe
emoji-keyboard <168225183+emoji-keyboard@users.noreply.github.com>
2024-04-26 16:25:20 -0500

    Add files via upload

---
+++ index_files/code.js
@@ -0,0 +1,48 @@
+
+
+function teeth(){
+    var audio = new Audio('index_files/teeth.mp3');
+    audio.play();
+};
+
+function shake(){
+    var audio = new Audio('index_files/shake.mp3');
+    audio.play();
+};
+
+function yawn(){
+    var audio = new Audio('index_files/yawn.mp3');
+    audio.play();
+};
+
+function sidekiss(){
+    var audio = new Audio('index_files/sidekiss.mp3');
+    audio.play();
+};
+
+function drool(){
+    var audio = new Audio('index_files/drool.mp3');
+    audio.play();
+};
+
+function sad(){
+    var audio = new Audio('index_files/sad.mp3');
+    audio.play();
+};
+
+function nomouth(){
+    var audio = new Audio('index_files/nomouth.mp3');
+    audio.play();
+};
+
+function hehe(){
+    var audio = new Audio('index_files/hehe.mp3');
+    audio.play();
+};
+
+function detective(){
+    var audio = new Audio('index_files/detective.mp3');
+    audio.play();
+};
+
+//    window.alert("sometext");
